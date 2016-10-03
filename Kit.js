31af581e7fc43801b2e9e53c5e42d88af4805a62
Sumanth Srinivasan <sumanth.srinivasan@gmail.com>
2016-10-03 14:12:04 -0400

    Loop functionality without time-sync

---
+++ Kit.js
@@ -0,0 +1,67 @@
+// Creates the instance of a drum kit and loads the drum hits that correspond to it
+function Kit(elementName1,elementName2,elementName3,elementName4,kitName) {
+
+
+	P1 = new Hit(elementName1,'audio/909/B.wav');
+    Q = new Hit(elementName2,'audio/909/H.wav');
+    // W = new Pad('#dropW');
+    // E = new Pad('#dropE');
+    // R = new Pad('#dropR');
+
+    A = new Hit(elementName3,'audio/909/CLP.wav');
+    // S = new Pad('#dropS');
+    // D = new Pad('#dropD');
+    // F = new Pad('#dropF');
+
+    Z = new Hit(elementName4,'audio/909/S.wav');
+
+
+
+
+}
+
+
+
+// Hit creates the instance of a drum hit and maps it to an HTML element
+function Hit(elementName,filePath){
+
+	
+	var element = select(elementName);
+
+	var sample = new Tone.Player(filePath).toMaster();
+	sample.setLoopPoints(0,1);
+	// sample.retrigger = true;
+	
+	// var seq = new Tone.Sequence(function(time){
+	// 		console.log(time);
+	// 		sample.start();
+	// 		// straight quarter notes
+	// 		}, "4n");
+
+	this.play = function(keyCount) {
+
+		if (keyCount === 1){
+			sample.start();
+			element.style('background-color','#cfc');
+
+		}
+		else {
+			
+			// seq.start();
+			sample.loop = true;
+			sample.start();
+
+			// var padLoop = new Tone.Loop(function(time){
+			// 	//triggered every eighth note. 
+			// 	console.log(time);
+			// 	sample.start();
+			// }, "8n");
+			// loop.start();
+		}
+
+	}
+	this.stop = function() {
+		element.style('background-color','#ccc');
+		sample.stop();
+	}
+}
--- main_script.js
+++ main_script.js
@@ -1,64 +1,152 @@
 
 var Q,W,E,R,A,S,D,F,Z,X,C,V;
 
+// Flags
+
+var keyCount = 0;
+var kitFlag = true;
+
+function toggle(button) 
+{
+     switch(button.value)
+     {
+          case "CUSTOM":
+               button.value = "909 KIT";
+               break;
+          case "909 KIT":
+               button.value = "CUSTOM";
+               break;
+     }
+}
+
 function setup() {
 	createCanvas(1,1);
 	background(1);
 
+
 	// Initialize all the Pads in the sampler
 
-	Q = new Pad('#dropQ');
-	W = new Pad('#dropW');
-	E = new Pad('#dropE');
-	R = new Pad('#dropR');
 
-	A = new Pad('#dropA');
-	S = new Pad('#dropS');
-	D = new Pad('#dropD');
-	F = new Pad('#dropF');
+  if (kitFlag == false) {
 
-	Z = new Pad('#dropZ');
-	X = new Pad('#dropX');
-	C = new Pad('#dropC');
-	V = new Pad('#dropV');
+    P1 = new Pad('#drop1');
+
+    Q = new Pad('#dropQ');
+    // W = new Pad('#dropW');
+    // E = new Pad('#dropE');
+    // R = new Pad('#dropR');
+
+    A = new Pad('#dropA');
+    // S = new Pad('#dropS');
+    // D = new Pad('#dropD');
+    // F = new Pad('#dropF');
+
+    Z = new Pad('#dropZ');
+    // X = new Pad('#dropX');
+    // C = new Pad('#dropC');
+    // V = new Pad('#dropV');
+
+  }
+  else {
+
+    kit = new Kit('#drop1','#dropQ','#dropA','#dropZ','909');
+  }
+
+	
 }
 
 //QWERTY interface and keyboard input mapping
 function keyTyped() {
 
-
-  
-  if (key === 'q' || key === 'Q') {
-    Q.play();
+  if (keyCount < 5){
+    keyCount = keyCount + 1;
+  }
+  if (key === '1'){
+    P1.play(keyCount);
+  }
+    else if (key === 'q' || key === 'Q') {
+    Q.play(keyCount);
   } else if (key === 'w' || key === 'W') {
-    W.play();
+    W.play(keyCount);
   } else if (key === 'e' || key === 'E') {
-    E.play();
+    E.play(keyCount);
   } else if (key === 'r' || key === 'R') {
-    R.play();
+    R.play(keyCount);
   } 
 
   	else if (key === 'a' || key === 'A') {
-    A.play();
+    A.play(keyCount);
   } else if (key === 's' || key === 'S') {
-    S.play();
+    S.play(keyCount);
   } else if (key === 'd' || key === 'D') {
-    D.play();
+    D.play(keyCount);
   } else if (key === 'f' || key === 'F') {
-    F.play();
+    F.play(keyCount);
   } 
 
   	else if (key === 'z' || key === 'Z') {
-    Z.play();
+    Z.play(keyCount);
   } else if (key === 'x' || key === 'X') {
-    X.play();
+    X.play(keyCount);
   } else if (key === 'c' || key === 'C') {
-    C.play();
+    C.play(keyCount);
   } else if (key === 'v' || key === 'V') {
-    V.play();
+    V.play(keyCount);
   }
 }
 
+function keyReleased() {
+
+
+
+  if (keyCount < 5){
+    keyCount = keyCount + 1;
+  }
+  if (key === '1'){
+    P1.stop();
+  }
+    else if (key === 'q' || key === 'Q') {
+    Q.stop();
+  }
+  // } else if (key === 'w' || key === 'W') {
+  //   W.play(keyCount);
+  // } else if (key === 'e' || key === 'E') {
+  //   E.play(keyCount);
+  // } else if (key === 'r' || key === 'R') {
+  //   R.play(keyCount);
+  // } 
+
+    else if (key === 'a' || key === 'A') {
+      A.stop();
+  } 
+  //   else if (key === 's' || key === 'S') {
+  //   S.play(keyCount);
+  // } else if (key === 'd' || key === 'D') {
+  //   D.play(keyCount);
+  // } else if (key === 'f' || key === 'F') {
+  //   F.play(keyCount);
+  // } 
+
+    else if (key === 'z' || key === 'Z') {
+    Z.stop();
+  } 
+  // else if (key === 'x' || key === 'X') {
+  //   X.play(keyCount);
+  // } else if (key === 'c' || key === 'C') {
+  //   C.play(keyCount);
+  // } else if (key === 'v' || key === 'V') {
+  //   V.play(keyCount);
+  // }
+
+  keyCount = 0;
+  
+  
+
+  
+}
+
+
 // Enable Audio
 
-Tone.Transport.start(); //Play audio
+Tone.Transport.start();
+// Tone.Transport.loop = true; //Play audio
